d994395c0960eea96a4ccabb89071cd327f23c74
sakshi <19sept.sakshi2002@gmail.com>
2022-07-04 20:40:08 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const express=require('express')
+const bodyParser =require('body-parser')
+
+const app=express()
+const port=3000
+//Static Files
+app.use(express.static('public'))
+app.use('/css',express.static(__dirname + 'public/css'))
+app.use('/img',express.static(__dirname + 'public/img'))
+app.use('/js',express.static(__dirname + 'public/js'))
+
+//Templating Engine
+app.set('views','./src/views')
+app.set('view engine','ejs')
+
+app.use(bodyParser.urlencoded({extended : false}))
+
+//Routes
+const newsRouter=require('./src/routes/news')
+
+app.use('/',newsRouter)
+app.use('/article',newsRouter)
+
+// Listen on port 3000
+app.listen(port, () => console.log(`Listening on port ${port}`))
---
+++ src/routes/news.js
@@ -0,0 +1,70 @@
+const express = require('express')
+const newsRouter = express.Router()
+const axios = require('axios')
+
+newsRouter.get('/', async(_req, res) => {
+    try {
+        const newsAPI = await axios.get(`https://newsapi.org/v2/everything?q=tesla&from=2022-06-04&sortBy=publishedAt&apiKey=5e4d186d03f946cfbc2a49d1c19c6288`)
+        res.render('news', { articles : newsAPI.data.articles })
+    } catch (err) {
+        if(err.response) {
+            console.log(err.response.data)
+            console.log(err.response.status)
+            console.log(err.response.headers)
+            res.render('news', { articles : null })
+        } else if(err.request) {
+            res.render('news', { articles : null })
+            console.log(err.request)
+        } else {
+            res.render('news', { articles : null })
+            console.error('Error', err.message)
+        }
+    } 
+})
+
+newsRouter.get('/:id', async(req, res) => {
+    let articleID = req.params.id
+
+    try {
+        const newsAPI=await axios.get(`https://newsapi.org/v2/everything?q=${articleID}&tesla&from=2022-06-04&sortBy=publishedAt&apiKey=5e4d186d03f946cfbc2a49d1c19c6288`)
+       
+        res.render('newsSingle', { article : newsAPI.data })
+    } catch (err) {
+        if(err.response) {
+            res.render('newsSingle', { article : null })
+            console.log(err.response.data)
+            console.log(err.response.status)
+            console.log(err.response.headers)
+        } else if(err.request) {
+            res.render('newsSingle', { article : null })
+            console.log(err.request)
+        } else {
+            res.render('newsSingle', { article : null })
+            console.error('Error', err.message)
+        }
+    } 
+})
+
+
+newsRouter.post('', async(req, res) => {
+    let search = req.body.search
+    try {
+        const newsAPI = await axios.get(`http://newsapi.org/v2/everything?q=${search}&apiKey=5e4d186d03f946cfbc2a49d1c19c6288`)
+        res.render('newsSearch', { articles : newsAPI.data.articles })
+    } catch (err) {
+        if(err.response) {
+            res.render('newsSearch', { articles : null })
+            console.log(err.response.data)
+            console.log(err.response.status)
+            console.log(err.response.headers)
+        } else if(err.requiest) {
+            res.render('newsSearch', { articles : null })
+            console.log(err.requiest)
+        } else {
+            res.render('newsSearch', { articles : null })
+            console.error('Error', err.message)
+        }
+    } 
+})
+
+module.exports = newsRouter 
