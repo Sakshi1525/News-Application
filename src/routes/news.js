baa45c63779064f5884420d45fdf3b2cd022fa2a
sakshi <19sept.sakshi2002@gmail.com>
2022-07-05 12:00:50 +0530

    make it better

--- src/routes/news.js
+++ src/routes/news.js
@@ -4,7 +4,7 @@ const axios = require('axios')
 
 newsRouter.get('/', async(_req, res) => {
     try {
-        const newsAPI = await axios.get(`https://newsapi.org/v2/everything?q=tesla&from=2022-06-04&sortBy=publishedAt&apiKey=5e4d186d03f946cfbc2a49d1c19c6288`)
+        const newsAPI = await axios.get(`https://newsapi.org/v2/everything?language=en&q=tesla&from=2022-06-04&sortBy=publishedAt&apiKey=5e4d186d03f946cfbc2a49d1c19c6288`)
         res.render('news', { articles : newsAPI.data.articles })
     } catch (err) {
         if(err.response) {
@@ -26,7 +26,7 @@ newsRouter.get('/:id', async(req, res) => {
     let articleID = req.params.id
 
     try {
-        const newsAPI=await axios.get(`https://newsapi.org/v2/everything?q=${articleID}&tesla&from=2022-06-04&sortBy=publishedAt&apiKey=5e4d186d03f946cfbc2a49d1c19c6288`)
+        const newsAPI=await axios.get(`https://newsapi.org/v2/everything?language=en&q=${articleID}&tesla&from=2022-06-04&sortBy=publishedAt&apiKey=5e4d186d03f946cfbc2a49d1c19c6288`)
        
         res.render('newsSingle', { article : newsAPI.data })
     } catch (err) {
@@ -49,7 +49,7 @@ newsRouter.get('/:id', async(req, res) => {
 newsRouter.post('', async(req, res) => {
     let search = req.body.search
     try {
-        const newsAPI = await axios.get(`http://newsapi.org/v2/everything?q=${search}&apiKey=5e4d186d03f946cfbc2a49d1c19c6288`)
+        const newsAPI = await axios.get(`http://newsapi.org/v2/everything?language=en&q=${search}&apiKey=5e4d186d03f946cfbc2a49d1c19c6288`)
         res.render('newsSearch', { articles : newsAPI.data.articles })
     } catch (err) {
         if(err.response) {
